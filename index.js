4f38660c168a64324b27d1bb9a43db48a7bce370
sunarya-thito <septogeddon@gmail.com>
2021-02-01 21:13:55 +0700

    .

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const port = process.env.PORT || 5000;
 const server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 const cache = {};
-const cacheUsername = {};
+
+app.get('/mapel', (req, res) => {
+	//https://cbtsman1banjar.my.id/wp-content/themes/unbk/api-18575621/getmapel.php
+	let options = {
+		hostname: 'cbtsman1banjar.my.id',
+		port: 443,
+		path: '/wp-content/themes/unbk/nilai.php',
+		method: 'GET'
+	};
+	let request = https.request(options, result => {
+		const chunks = [];
+		result.on('data', data => chunks.push(data));
+		result.on('end', () => {
+			const resultData = Buffer.concat(chunks).toString();
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.send(resultData);
+		});
+	});
+	request.end();
+});
 
 app.get('/nilai', (req, res) => {
 	let userid = req.query.userid;
