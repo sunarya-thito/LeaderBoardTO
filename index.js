b4c462dc4701c2d08564681b4077b4602267b41e
sunarya-thito <septogeddon@gmail.com>
2021-02-01 11:16:11 +0700

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const querystring = require('querystring');
 const https = require('https');
-const request = require('request');
 const app = express();
 const port = process.env.PORT || 5000;
 const server_host = process.env.YOUR_HOST || '0.0.0.0';
@@ -9,23 +9,26 @@ app.get('/nilai', (req, res) => {
 	let userid = req.query.userid;
 	let mapel = req.query.mapel;
 	console.log('nilai: '+userid+' dari '+mapel);
-	request({
-	    headers: {
-	        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
-	        'Content-Type': 'application/x-www-form-urlencoded'
-	    },
-	    url: 'https://cbtsman1banjar.my.id/wp-content/themes/unbk/nilai.php',
-	    body: {
-	        'userid': '' + userid,
-	        'mapel': '' + mapel
-	    },
-	    method: 'POST'
-	}, (error, result, body) => {
-	    console.log('out:');
-	    if (error) console.log('error', error);
-	    if (result) console.log('result', result);
-	    if (body) console.log('body', body);
+	let options = {
+		hostname: 'cbtsman1banjar.my.id',
+		port: 443,
+		path: '/wp-content/themes/unbk/nilai.php',
+		method: 'POST'
+	};
+	let request = https.request(options, result => {
+	    const chunks = [];
+	    result.on('data', data => {
+	        chunks.push(data);
+	    });
+	    result.on('end', () => {
+	        const resultData = Buffer.concat(chunks).toString();
+	    });
 	});
+	request.write(querystring.stringify({
+	    'userid': userid,
+	    'mapel': mapel
+	}));
+	request.end();
 });
 
 app.listen(port, server_host, () => console.log(`Application listening on port ${port}!`))
